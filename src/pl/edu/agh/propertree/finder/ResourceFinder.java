73046c8a421cb84d3b9e49cc614d69c431391da7
Tomasz Kasprzyk <kasptom@gmail.com>
2017-05-08 19:47:44 +0200

    Initial commit

---
+++ src/pl/edu/agh/propertree/finder/ResourceFinder.java
@@ -0,0 +1,5 @@
+package pl.edu.agh.propertree.finder;
+
+public interface ResourceFinder {
+    public Object getResource(int resourceId);
+}
---
+++ src/pl/edu/agh/propertree/validator/PostfixValidator.java
@@ -0,0 +1,4 @@
+package pl.edu.agh.propertree.validator;
+
+public class PostfixValidator {
+}
